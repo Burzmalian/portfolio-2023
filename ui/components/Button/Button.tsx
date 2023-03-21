548c739059d5c2963517a6dc9a70451be4f14b63
gylesfohl <gyles.fohl@thomsonreuters.com>
2023-03-20 23:49:11 -0500

    test update

--- ui/components/Button/Button.tsx
+++ ui/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 const Button = () => (
-  <div className="text-xl text-[pink] bg-slate-400">Button</div>
+  <div className="text-xl text-[red] bg-slate-400">Button</div>
 );
 
 export default Button;
