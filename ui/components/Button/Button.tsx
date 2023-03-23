3c018226a782b9c4f36940941b07f234b35d9d13
gylesfohl <gyles.fohl@thomsonreuters.com>
2023-03-22 20:55:15 -0500

    commit test

--- ui/components/Button/Button.tsx
+++ ui/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 const Button = () => (
-  <div className="text-xl text-[red] bg-slate-400">Button</div>
+  <div className="bg-slate-400 text-xl text-[red]">Button</div>
 );
 
 export default Button;
