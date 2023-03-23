3e37186220f587f1128f4920f081b52e127c1019
gylesfohl <gyles.fohl@thomsonreuters.com>
2023-03-22 23:39:33 -0500

    setup unit testing ci/precommit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,7 @@ module.exports = {
   "*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)": [
     `pnpm run format`,
     `pnpm run lint`,
+    `pnpm run typecheck`,
+    `pnpm run test:ci`,
   ],
 };
